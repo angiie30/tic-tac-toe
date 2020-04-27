cd8aa7ace45424689fa17e35dd8dd30c8d1c7ec7
Angélica Rosario <arosario@halfkib.com>
2020-04-27 15:36:04 -0400

    validation for show time travel

--- src/app/game/time-travel.js
+++ src/app/game/time-travel.js
@@ -8,7 +8,7 @@ function TimeTravel(props) {
     return (
       <div key={move} className="list-group">
         <button
-          className="list-group-item list-group-item-action text-center"
+          className="list-group-item list-group-item-action pointer"
           onClick={() => props.onClick(move)}
         >
           {desc}
@@ -17,18 +17,20 @@ function TimeTravel(props) {
     );
   });
 
-  return (
-    <div className="col-sm-12 col-md-12 col-lg-4">
-      <div className="card box-shadown mt-3">
-        <div className="card-body">
-          <div className="card-title">
-            <h5>Time Travel</h5>
+  if (moves.length <= 1) return "";
+  else
+    return (
+      <div className="col-sm-12 col-md-12 col-lg-4">
+        <div className="card box-shadown mt-3">
+          <div className="card-body">
+            <div className="card-title">
+              <h5>Time Travel</h5>
+            </div>
           </div>
+          {moves}
         </div>
-        {moves}
       </div>
-    </div>
-  );
+    );
 }
 
 export default TimeTravel;
