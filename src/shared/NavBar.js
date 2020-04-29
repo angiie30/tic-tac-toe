52302eba300432c3ba0477b8417b74904c49358e
Angélica Rosario <arosario@halfkib.com>
2020-04-29 09:15:24 -0400

    logo in nav bar

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -1,18 +1,19 @@
 import React from "react";
+import logo from "../assets/img/logo.png";
 
 export default function NavBar() {
   return (
     <div className="nav-bar">
       <nav className="navbar navbar-dark bg-primary">
         <a className="navbar-brand" href="/#">
-          {/* <img
-              src="logo.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt=""
-            ></img> */}
-          Tic Tac Toe
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+          />
+          <span className="ml-2">Tic Tac Toe</span>
         </a>
       </nav>
     </div>
